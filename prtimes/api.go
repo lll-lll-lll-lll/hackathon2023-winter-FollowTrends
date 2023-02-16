53e430e454be4f8fd975093b5d5f76613f3225b1
0xpeIpeI <hokumaru12@gmail.com>
2023-02-16 19:08:33 +0900

    add #6 prtimesとlineの繋ぎこみ

--- prtimes/api.go
+++ prtimes/api.go
@@ -1,28 +1,47 @@
+// PRTimes API周りの処理全般を担当
 package prtimes
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 )
+
+// PRTimes APIを叩いたり、周りの処理を担当する
+type PRTimes struct {
+	Response Items `json:"response"`
+}
+
+func New() *PRTimes {
+	return &PRTimes{}
+}
 
 type Item struct {
 	Title string `json:"title"`
 	URL   string `json:"url"`
 }
 
-// PRTimes APIを叩いたり、周りの処理を担当する
-type PRTimes struct {}
+// String LINE上に表示するフォーマット
+func (i Item) String() string {
+	return fmt.Sprintf("%s:%s", i.Title, i.URL)
+}
 
-func New()*PRTimes {
-	return &PRTimes{}
+type Items []Item
+
+// String スライスのItem構造体を文字列にするメソッド
+func (i Items) String() string {
+	var s string
+	for _, v := range i {
+		s += v.String()
+	}
+	return s
 }
 
 // GetItems PRTimes APIを叩いてItem構造体を返す
-func (pt *PRTimes)GetItems(categoryID int) ([]Item, error) {
+func (pt *PRTimes) GetItems(categoryID string) (Items, error) {
 	client := &http.Client{}
-	url := "https://hackathon.stg-prtimes.net/api/categories/5/releases?from_date=2023-02-13"
+	url := fmt.Sprintf("https://hackathon.stg-prtimes.net/api/categories/%s/releases?from_date=2023-02-13", categoryID)
 	req, err := http.NewRequest("GET", url, nil)
 
 	req.Header.Set("Authorization", "Bearer b655dffbe1b2c82ca882874670cb110995c6604151e1b781cf5c362563eb4e12")
@@ -36,9 +55,10 @@ func (pt *PRTimes)GetItems(categoryID int) ([]Item, error) {
 	if err != nil {
 		return nil, err
 	}
-	var items []Item
+	var items Items
 	if err := json.Unmarshal(body, &items); err != nil {
 		return nil, err
 	}
+	pt.Response = items
 	return items, nil
 }
