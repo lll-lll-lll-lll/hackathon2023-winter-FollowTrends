3856565bd40d03015f6260bb1823e97d1c6cfbbf
joljolseizinn <115388853+joljolseizinn@users.noreply.github.com>
2023-02-17 05:31:45 +0900

    change database

--- db/init/TestData.sql
+++ db/init/TestData.sql
@@ -1,7 +1,15 @@
-CREATE TABLE TEST_USER (
-                           id BIGINT PRIMARY KEY,
-                           name VARCHAR(20) NOT NULL
+CREATE TABLE  Test_DATA(
+                           keyword varchar(255) PRIMARY,
+                           url varchar(255) NOT NULL
 );
 
-INSERT INTO TEST_USER VALUES(1, 'user_1');
-INSERT INTO TEST_USER VALUES(2, 'user_2');
+INSERT INTO TEST_USER VALUES('KDDI株式会社,音楽ライブ,地域経済,04 Limited Sazabys,BiSH,自治体,開催', 'https://prtimes.jp/main/html/rd/p/000000026.000081361.html');
+INSERT INTO TEST_USER VALUES('アメリカン・ニューシネマ,アーサー・ペン監督,青春群像劇,『アリスのレストラン』,Blu-ray', 'https://prtimes.jp/main/html/rd/p/000001180.000031422.html');
+INSERT INTO TEST_USER VALUES('Windows,0x80070103,アップデート,互換性,リセット,4DDiG Windows Boot Genius,Tenorshare,ファイル復元,重複ファ','https://prtimes.jp/main/html/rd/p/000000271.000035551.html')
+INSERT INTO TEST_USER VALUES('ホロライブ,みんなで空気読み。オンライン,紫咲シオン,桃鈴ねね,尾丸ポルカ,Nintendo','https://prtimes.jp/main/html/rd/p/000000683.000001474.html')
+INSERT INTO TEST_USER VALUES('AutoReserve for Restaurants 予約台帳,AI,電話予約代行システム,テクノロジー,キャンセル料徴収','https://prtimes.jp/main/html/rd/p/000000009.000056009.html')
+INSERT INTO TEST_USER VALUES('Teclast,T50,Android 12,UNISOC T616,Mail-G52,11インチ2k,8GB RAM,128GB,Micro SD,ジャイロスコープ,デュアルSIM,TD-LTE','https://prtimes.jp/main/html/rd/p/000000222.000080488.html')
+INSERT INTO TEST_USER VALUES('KATE,カメレオンパレード,バランシングカラーパレット,メタリックジュエリーリキッド,ギャラクシーモードアイズ,オーロラライズド','https://prtimes.jp/main/html/rd/p/000000523.000016220.html')
+INSERT INTO TEST_USER VALUES('LEGO,BTS Dynamite,YouTube,No.1 single,749 pieces,RM,Jin,SUGA,j-hope,Jimin,V,Jung Kook,Jacob,Josh, Federico Begher,Pick a Brick,Build a Minifigure,AR','https://prtimes.jp/main/html/rd/p/000000130.000048417.html')
+INSERT INTO TEST_USER VALUES('バーガーキング®,チーズバーガー・ゲレンデ,直火焼きの100%ビーフパティ4枚,ゴーダチーズスライス6枚','https://prtimes.jp/main/html/rd/p/000000198.000038980.html')
+INSERT INTO TEST_USER VALUES('GeForce RTX™ 4070 Ti,NVIDIA Ada Lovelaceアーキテクチャ,RTコア,Tensorコア,ストリーミングマルチプロセッサー,レイトレーシング,AI,GDDR6X','https://platform.openai.com/playground?model=text-davinci-003')
