af6751b8be9bdd8e33f0e81256534ace03bca953
yuporon <ftb.y.m.kashima.07@gmail.com>
2023-02-17 15:11:18 +0900

    通知機能処理作成（毎日18:00に通知）

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -1,16 +1,20 @@
 import * as functions from "firebase-functions";
+import axios from "axios";
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
 
 export const notify = functions.region("asia-northeast1")
-    .pubsub.schedule("every 10 minutes")
+    .pubsub.schedule("every day 18:00")
     .timeZone("Asia/Tokyo")
-    .onRun((context) => {
-      console.log("Hello World!");
+    .onRun(async () => {
+      console.log("notify");
+      // localhost:5004/notify をここで叩く
+      const res = await axios.get("https://930e-221-247-249-83.jp.ngrok.io/notify");
+      console.log(res.data);
     });
